525f9f75a34b576fb4dfae78e314e1a83ed1f50f
jlesly <leslyj2@gmail.com>
2022-01-16 22:29:03 -0500

    created form handler

--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ const users_endPoint = "http://localhost:3000/api/v1/users"
 const questions_endPoint = "http://localhost:3000/api/v1/questions"
 
 document.addEventListener('DOMContentLoaded', () => {
-    getCategories()
+    getCategories(), getUsers(), getQuestions()
+
+    const createNewUserForm = document.getElementById("new-user")
+
+    createNewUserForm.addEventListener("submit", createFormHandler)
 })
 
 function getUsers() {
@@ -18,13 +22,31 @@ function getUsers() {
         })
 }
 
+function getUsers() {
+    fetch(users_endPoint)
+        .then(response => response.json())
+        .then(json => {
+            for (let user of json.data) {
+                let userData = Object.assign({}, { id: user.id }, user.attributes)
+                let newUser = new user(userData)
+                newUser.renderUser()
+            }
+        })
+}
 
+function createFormHandler(e) {
+    e.preventDefault()
+    let newUser = document.querySelector("#create-user").value
+    let score = document.querySelector("#score").value
+    postFetch(newUser)
+}
 
 function getCategories() {
     fetch(categories_endPoint)
         .then(response => response.json())
         .then(json => {
             for (let category of json.data) {
+
                 let newCategory = new Category(category, category.attributes)
             }
             Category.renderCategory();
